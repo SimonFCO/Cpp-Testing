a55439714094ca399e4b7b9ca359271beb14e66a
simonFCO <simon.vare6@gmail.com>
2025-12-31 00:10:16 +0100

    Added a max to how many attempts one gets

--- C++Testing/C++Testing.cpp
+++ C++Testing/C++Testing.cpp
@@ -7,13 +7,23 @@
 int main() {
     srand(time(0));
     int ComputerGuess = rand() % 10;
+    int AnswerAmmount = 6;
     std::string answer;
     Utilities utils;                     // create an instance
+    std::cout << "Welcome To This AMAZING Number Guesser\n";
+    std::cout << "Simply enter a number and the Computer will tell you if it was too high or low\n";
+    std::cout << "You Only Get 5 Tries\n";
+    std::cout << "Good Luck :)\n";
 
 
     int answer2 = 1;
-    while (answer2 != 0) {
+    while (answer2 != 0 && AnswerAmmount != 1) {
+        AnswerAmmount -= 1;
+        std::cout << "\n";
         std::cout << "Enter a number: ";
+        std::cout << "You Got ";
+        std::cout << AnswerAmmount;
+        std::cout << " attempts left :)";
         std::getline(std::cin, answer);
 
         int value;
@@ -28,4 +38,8 @@ int main() {
         std::cout << "You entered: " << value << '\n';
         answer2 = utils.CheckComputersGuess(value, ComputerGuess);
     } 
+
+    if (AnswerAmmount == 1) {
+        std::cout << "No more attempts, nice try";
+    }
 }
