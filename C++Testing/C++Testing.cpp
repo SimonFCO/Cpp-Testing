f867f8844fa958dadeba6ccfb135f2d716329507
simonFCO <simon.vare6@gmail.com>
2026-01-01 15:54:50 +0100

    Setup Done

--- C++Testing/C++Testing.cpp
+++ C++Testing/C++Testing.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "Utilities.h"
 #include "NumberGuesser.h"
+#include "Calculator.h"
 
 int main() 
 {
 	NumberGuesserStart();
+	Calculator();
 }
---
+++ C++Testing/Calculator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Utilities.h"
+
+float Calulator()
+{
+	float answer = 0;
+
+	return answer;
+}
